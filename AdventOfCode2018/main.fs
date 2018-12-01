2c9fcf631f0ea396a6094190e2625c5e56e1537a
suXin <suxinjke@gmail.com>
2018-12-01 16:49:32 +0700

    day 1

---
+++ AdventOfCode2018/main.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let answer =
+        match Array.toList argv with
+        | [ "1" ] -> day01.testAndSolve
+        | [ "2" ] -> day01.testAndSolve2
+        | _ -> "no argument given"
+
+    Console.WriteLine answer
+    
+    Console.ReadKey() |> ignore
+
+    0
