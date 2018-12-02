5115108ea6f1011d710e4eba8891ca0bdb95fd60
suXin <suxinjke@gmail.com>
2018-12-02 23:31:12 +0700

    day 02 fail

---
+++ AdventOfCode2018/day02.fs
@@ -0,0 +1,305 @@
+﻿module day02
+
+let input = "omlvgpokxfnctqyersabjwzizp
+omlvtdhxxflctqyersabjwziup
+omlvgdakxfnctqyersabzmziup
+omlvgdhkxfnchqyersarjwsiup
+omlvgdnkxfnctqyersabhwziuq
+omvlgdhkxfnctqyersajjwziup
+fmlvgdbkxfnctqyersabjwzqup
+omlvcdhexfnctqyersibjwziup
+omlvgdhkxfnctqyersoyjnziup
+omdbgdhpxfnctqyersabjwziup
+omlvgdbkxfnctiyersabjwziwp
+omlogdhkxfncpqyersabjfziup
+omlvgdhkxfncxayersabwwziup
+omlvgdhkxfgctqyepsabjnziup
+omlvzdhkxfnctqyerxabjwoiup
+orlvtdhoxfnctqyersabjwziup
+omgvgdhkxfnctqyetsarjwziup
+omlvgdhkxunctcqersabjwziup
+omlvgdhkxfnctqyertakjwziun
+omlvhdhkxfhetqyersabjwziup
+omlvjdhkxfnctqyersabjtzirp
+omsvgdhkifnctqyeryabjwziup
+ohlvgdhkxfncteyersabtwziup
+omlvgdhkxjqctqyerkabjwziup
+omljgdhkxfncxqiersabjwziup
+omlvgdhkxvnctqyetscbjwziup
+omlvgdhxxfnctqykrsabjwziui
+omlbgdhkxfnetqyersabjwliup
+omlvgvhkxfnctqyerjabjwzwup
+wmlvgdhkxfnctqyyrsabjwziuc
+omlvgdhkufnctqxersabjwpiup
+omlvgdtkxfnctqyercvbjwziup
+omtvgdhkxfnctqygrsmbjwziup
+omlvgdbkxfnctqyersagjwzrup
+omlvgdpksfnctqyorsabjwziup
+omlvgdlkxfnctqyerhaajwziup
+omlvgdhkxfnctqyersabjwkiqh
+omlvgdykxfnctqdersdbjwziup
+omligdhklfnctpyersabjwziup
+omlvzdhkxfnctryersabjwziap
+nmlvgdqkxfnctqyemsabjwziup
+omlvgdhkxoncqqyersabjyziup
+otlvgdhkxfnctqyersabjwzzhp
+omlvgdhvxfnctqyirsabjwziue
+omlvgohkxfnctqjersabjwzeup
+omlngdhkxfnytqyersabjwsiup
+gmlvgbhkxfnctqyersabjwziyp
+nmlvgxhkxfnctqyxrsabjwziup
+omlvwdhkufnctqyerfabjwziup
+omlvqdhkxfnctqyersabfmziup
+omlvgdhkxfnctqlerscbjeziup
+omlvgdhkxfncxqyerjabjgziup
+omlvgdhkxwnctqyersvbjwriup
+ozlvgdhkxfnctqyersabjjziuj
+omlvguhkxfnctqyersabjwznut
+ozlvwdhkxfactqyersabjwziup
+oplvgdhkxfnctqyersakjwiiup
+omlkgbhkxfnctqyetsabjwziup
+oukvgdhkxfnctqyerslbjwziup
+omllgwhkxfnctqyersasjwziup
+omlvgdqkvfnctqyjrsabjwziup
+omlvguhkxfnctqyepsakjwziup
+oblvgdhkxfnctqyersibjwciup
+omlvgdhkxfpetqyersnbjwziup
+omlvgdhkxfnctqyersabgwpmup
+ohlvgdhkxfnctqyersgbjwdiup
+omlvgkhkxfnctqyarsabjwziuj
+omtvgdhkxfnctqoersabjwzfup
+omlvgdhkxfncbqyersafjwzkup
+amlvgdhkmfnctqyorsabjwziup
+omlvndhkxfnctbyersagjwziup
+oslvgdhkxfactqyersabjwziip
+omlvgdhkxfnrtqyerumbjwziup
+omjvgdhaxfnctqyersajjwziup
+omlvgdhkxfyctqyersabjvziuf
+omlvgdhkxfgctqyervabjwzuup
+oclvhdhkxfnctqyirsabjwziup
+omlvgdhkxfnctqyrrsbbjwsiup
+nmlvghhkxfnctqyersayjwziup
+omlvgdhksfnzcqyersabjwziup
+omlvgdhbxknctqyerzabjwziup
+omlvgdhsxflctqyercabjwziup
+omlvgdhkxfncthyersabjpzgup
+omlvgdhkxfnhtqyersadjwzilp
+omlvgdhyxfnctqyershjjwziup
+omlvhdhkxfnctqytesabjwziup
+omlvgbhkxfnctqyhrsabjwmiup
+omlvnyhkxfnctqyersabbwziup
+omlvgdhkxfnhzqcersabjwziup
+omljgdhkvynctqyersabjwziup
+omrvgdhkxfnctqysrsabjmziup
+omlvgdhgxenctqyerfabjwziup
+omlvgdokxfncvqyersasjwziup
+omlvguhkxfnctqyersabbbziup
+imkvgdhkxfnctqyqrsabjwziup
+omlvgdikxfnctwyersabbwziup
+oulvgdhuxfngtqyersabjwziup
+omlvgdhkxfdctqqbrsabjwziup
+omlvgdhbofnctqyersmbjwziup
+omlzgdhkxfnctzyecsabjwziup
+oflvgdhkxfnctqyerpabjwzcup
+ommvgdhkxfnctqyicsabjwziup
+omlvgdhkxfnctqyewsabjwzisd
+ojlvgdhfxfnctqyersabjwzihp
+smlvgdhkxfnctqyzrsabjwaiup
+ohlvgdhkxfnctqyersabnwziue
+jslvgdhkxfnctqdersabjwziup
+omlvgdhkdenctwyersabjwziup
+orljgdhkxbnctqyersabjwziup
+omlvgdhkxfnctaaersabjwzrup
+qmlvgdhknfncqqyersabjwziup
+omlvgdhkxfnctqyerssbjwncup
+omlvgdhkxynctqdercabjwziup
+omivgdhpxfnctqiersabjwziup
+omuvgdbkxfnctqyersajjwziup
+omlvbdokxfnctqyehsabjwziup
+gmlvgdhkxcnctqyemsabjwziup
+hmlvgdhkxfncsqyersabjwzidp
+omlvgdhkxftztqytrsabjwziup
+omlvgdhkxfnatqyeesabjbziup
+omlvodhkxfnctqbirsabjwziup
+omlvgdhsifnctqyersabjwziop
+oyvvgdhkxfnctqyersabjwzinp
+qmlvgdhkxfnctqyersdbawziup
+omlvguhkxfncuqyersabjwzipp
+omspgdhkxfnctqyersabjwzifp
+omlvgdhkxfnamqyeryabjwziup
+omlvgdhkngnctqyxrsabjwziup
+omdvcdhkxfnctqynrsabjwziup
+omyvgdhkxfnctqyeryabjyziup
+hmlvgdhkxfnctqyersabjwzwap
+ombvgdhkxfyctqyersabjwziuk
+omlvadhkxfnctqyersoqjwziup
+ollugdhkxfnctqyersabjwzizp
+omlvgdhkxfncvqmersabjwiiup
+omlvgdkkxfnupqyersabjwziup
+omlvgdhkxfncratersabjwziup
+oklvgdskxfnctqyersabjkziup
+omlvgdhkxfnctqyernebgwziup
+omsvgdhkxfnctqyersaejwziuv
+omlvgdhkxfrctlynrsabjwziup
+omlggdhkxfnctqyersbbjmziup
+omlvgdhfxfnctqyehrabjwziup
+omqvgdhkxfnctqcersabjwzfup
+omlvgdhklfncqxyersabjwziup
+omlvgxhkxfnctqyersabebziup
+omlfgdhkxfnctjyersabkwziup
+omlvgdhkxfnctqysrtabjwqiup
+omlvgdhkxfnltqaersabfwziup
+ofhvgdhkxfnctqyessabjwziup
+omlvpdekxfnctqyerscbjwziup
+omlvcdhkxlnbtqyersabjwziup
+omlvfdhkxfnctqyersabjwrnup
+omlvddhkxfncdqyersabjwziut
+omlvgdhkxfnctqxersabjhiiup
+omidgdhkxfnctqyeysabjwziup
+omlogdhkxfnptqyersabjwniup
+omlvgdhkxfnwthyersabjwziuz
+omevgdhkxgnctbyersabjwziup
+omlvgdhkxfnytqyersabjozuup
+omlvgvhkxfmctqyersabjwziuw
+oelvgdhkxfoctqyersadjwziup
+lmlvgdhkxfnctqeersabjwzisp
+omlvgdhkxfcctqyersasjwzibp
+gmlvgdhkyfnctqyersabjwziuz
+omlvgdhkxfnctslersabjwziuf
+omlvgehkxfnctqyeosabjwziyp
+otlggdhkxfjctqyersabjwziup
+bmjvgdhixfnctqyersabjwziup
+omlvgqhkxfnctqdezsabjwziup
+omlvgbhkxfnciqnersabjwziup
+omlvgdhlxfnctqydrsdbjwziup
+omlvgdhkxfncfqyersabjwxinp
+ymlvgdhkxfnctqyersabhwziui
+omdvgdhkxfnctqyersabjwxdup
+bmlvgdhkxfnwtwyersabjwziup
+dmlvgmhkxfnctqyxrsabjwziup
+omlvgdhkxbnntqyersabjiziup
+omlvgdhkmfnctlyersgbjwziup
+omlvgdhkxfnctqhersablwzixp
+ommvgdhkxfwctqyersabnwziup
+omlkgdhjxfnctqyersabjwjiup
+omlvgdhrxfnctqyeasabjnziup
+omvvgdhkxtnctqyersabjtziup
+omlvgdhkufgctqyersabfwziup
+omqvgwhkxfnctqyevsabjwziup
+oalvgdhkyfyctqyersabjwziup
+omlvgdhkxfnctqyefvabjwhiup
+jmlvgdakxfnctqyersabjwtiup
+gmlvgmhkxfnctqyersaqjwziup
+omlvgdhkxcnctqydrszbjwziup
+omlvgdhkxfnctxnersxbjwziup
+omlvgyhkxfnctqyersabjeaiup
+omlcgdhkxfncvqyersabjoziup
+omlvgdhkxfycttyercabjwziup
+omlmgdhkpsnctqyersabjwziup
+lmlvglhkxfnctqdersabjwziup
+omlvgdhxdfncoqyersabjwziup
+omlvgdhkxfnctqyersabjwkixv
+oplvgdhkxfnctiyersabjoziup
+omlvgdnkxfnctdyersebjwziup
+omlvguckxfnctqwersabjwziup
+omlvgdhojfnctqyersabjoziup
+opjvxdhkxfnctqyersabjwziup
+omevgdhkdflctqyersabjwziup
+omlvgilkxfncaqyersabjwziup
+omlvgdhkqfnctqyersabunziup
+dmlvgdhkxrnctqyerssbjwziup
+omlvgdzcxfnctqyersabjwniup
+omlvgdhkxfnctqyeraabpsziup
+omlvgdhkxfnctqlersabjtziul
+omlvgbhkxfnctqyeysabjwpiup
+omlvgdhvxfnmttyersabjwziup
+omlvgdhkxznctqyersabewziua
+oqlvgdhkxfnctqjersabjfziup
+omlvgdhkqfnctqyoysabjwziup
+omlvgdhkxfnctqylrzabbwziup
+oalvguhkxfnctqyersabawziup
+omlvgdokxfncvqyersasjlziup
+omlvgdhkcfnctqyersazjwzfup
+oslvgdhpxfnctqyhrsabjwziup
+omlvgdhkxfnotqcqrsabjwziup
+umlvgdhlxfnctqyersnbjwziup
+oxlvgdhkxfnktjyersabjwziup
+omlvgdhkxhncnqyersabjwzirp
+jmlvgdhkxfncfqyersabjwzqup
+omlvgdhkbfnutvyersabjwziup
+omhvgddkxfnctqyersabqwziup
+omlvgdukxfnbtqyersabjwzjup
+oylvndhkxfnctqversabjwziup
+omlvgdhkcfnctqyersamjwfiup
+omlvgdskxfnctqyerssbjgziup
+qmlvgdhkxfncxqyersabiwziup
+omlvghhkxfnctwyersaljwziup
+omlvgdhkpfnbtqyersnbjwziup
+omlvgthkxfnctnyersabjwziut
+omlvgdhkpfnctqyeisabjfziup
+omlvgdhrxrnctqyersabjwzigp
+omlvjdhkxfnctqyersabpwwiup
+omlvgdhkxfnctsyersabjwzixl
+amlvgdhktfnctqyersabfwziup
+oklvvdhkxfnctoyersabjwziup
+rmlvgdhkxfncwqyersabxwziup
+omlvgdhkxfnctqyersabiwzjfp
+omlvgehkxfnctqyersebjzziup
+omlvgdhkxfncaqyersabwwzoup
+omlvgdhkxfncjqyersanjwfiup
+omlvgdhkwfnctqyersqbjwziux
+omrvgdhjxfnctqyeksabjwziup
+omlvgdhkxfnctpyersaftwziup"
+
+let extractLetters( input: string ) =
+    let findLettersWithExactAmount letters count =
+        Seq.toList letters
+        |> List.groupBy ( fun chr -> chr )     
+        |> List.filter ( fun ( _, amount ) -> amount.Length = count )
+        |> List.map ( fun ( chr, amount ) -> chr )
+
+    ( findLettersWithExactAmount input 2, findLettersWithExactAmount input 3 )
+
+let solve( input: string ) =
+    let result =
+        input.Split( [| ", "; "\r\n" |], System.StringSplitOptions.RemoveEmptyEntries )
+        |> Seq.toList
+        |> List.map extractLetters
+        |> List.map( fun ( two, three ) -> ( ( if two.Length > 0 then 1 else 0 ), ( if three.Length > 0 then 1 else 0 ) ) )
+        |> List.fold( fun ( sum_two, sum_three ) ( two, three ) -> ( sum_two + two, sum_three + three ) ) ( 0, 0 )
+
+    fst result * snd result
+
+let testAndSolve =
+    let test1_result = extractLetters "abcdef" = ( [], [] )
+    let test2_result = extractLetters "bababc" = ( [ 'a' ], [ 'b' ] )
+    let test3_result = extractLetters "abbcde" = ( [ 'b' ], [] )
+    let test4_result = extractLetters "abcccd" = ( [], [ 'c' ] )
+    let test5_result = extractLetters "aabcdd" = ( [ 'a'; 'd' ], [] )
+    let test6_result = extractLetters "abcdee" = ( [ 'e' ], [] )
+    let test7_result = extractLetters "ababab" = ( [], [ 'a'; 'b' ] )
+
+    ( solve input ).ToString()
+    
+let test_data2 = "abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz"
+
+let distance( str1: string, str2: string ) =
+    let rec char_distance( str1: char list, str2: char list, different_letters: char list ) = 
+        match str1.Length with
+        | 0 -> List.distinct different_letters
+        | _ ->
+            let diff = if str1.Head <> str2.Head then [ str1.Head; str2.Head ] else []
+            char_distance( str1.Tail, str2.Tail, different_letters @ diff )
+
+    char_distance( Seq.toList str1, Seq.toList str2, [] )
+
+let testAndSolve2 =
+
+    // At this point I realised I don't enjoy functional programming
+    ""
--- AdventOfCode2018/main.fs
+++ AdventOfCode2018/main.fs
@@ -6,6 +6,8 @@ let main argv =
         match Array.toList argv with
         | [ "1" ] -> day01.testAndSolve
         | [ "2" ] -> day01.testAndSolve2
+        | [ "3" ] -> day02.testAndSolve
+        | [ "4" ] -> day02.testAndSolve2
         | _ -> "no argument given"
 
     Console.WriteLine answer
